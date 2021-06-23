5a1079f326f4acefd808209bd8fccee9a9c9d51f
Charles Adams <charleskellyadams@gmail.com>
2021-06-23 14:26:52 -0600

    Fix

--- week2/boxes/src/App.js
+++ week2/boxes/src/App.js
@@ -3,10 +3,10 @@ import Card from './components/Card'
 function App() {
   return (
     <div className="container">
-      <Card title="Title" subtitle="Sub Title" description="Hello World" index="0"/>
-      <Card title="Title" subtitle="Sub Title" description="Hello World" index="1"/>
-      <Card title="Title" subtitle="Sub Title" description="Hello World" index="2"/>
-      <Card title="Title" subtitle="Sub Title" description="Hello World" index="3"/>
+      <Card title="Title" subtitle="Sub Title" description="Hello World" color="red"/>
+      <Card title="Title" subtitle="Sub Title" description="Hello World" color="orange"/>
+      <Card title="Title" subtitle="Sub Title" description="Hello World" color="yellow"/>
+      <Card title="Title" subtitle="Sub Title" description="Hello World" color="green"/>
     </div>
   );
 }
--- week2/boxes/src/components/Card.js
+++ week2/boxes/src/components/Card.js
@@ -7,9 +7,10 @@ export class Card extends Component {
 
     render() {
         return (
-            <div className={`card card-style-${this.props.index}`}>
+            <div style={{backgroundColor: this.props.color }} className="card">
                 <h1>{this.props.title} {this.props.index}</h1>
                 <h2>{this.props.subtitle} {this.props.index}</h2>
+                <hr style={{width: "100%"}}></hr>
                 <p>{this.props.description} {this.props.index}</p>
             </div>
         )
